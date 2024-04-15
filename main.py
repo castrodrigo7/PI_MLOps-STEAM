d9fe52ea11e3f90b76c01d2c75f1b06376b134ae
castrodrigo7 <castrodry7@egmail.com>
2024-04-15 16:52:23 -0300

    segundo endpoint

--- main.py
+++ main.py
@@ -33,3 +33,34 @@ def play_time_genre(genero: str):
     year_max_playtime = df_merged.groupby('year')['playtime_forever'].sum().idxmax()
 
     return {"Año de lanzamiento con más horas jugadas para el género {0}".format(genero) : int(year_max_playtime)}
+
+@app.get('/user_for_genre/{genero}')
+def UserForGenre(genero: str):
+    # Cargar los datos dentro de la función
+    cols_1 = ['id', genero, 'year']
+    df = pd.read_parquet('datasets/dfgames.parquet', columns=cols_1)
+    cols_2 = ['user_id', 'item_id', 'playtime_forever']
+    df_items = pd.read_parquet('datasets/users_item.parquet', columns=cols_2)
+    # Verifica si el género especificado es una columna en el DataFrame
+    if genero not in df.columns:
+        return {"error": "El género especificado no existe en los datos"}
+
+    # Filtra el DataFrame por el género especificado
+    df_genre = df[df[genero] == 1]
+
+    # Une df_genre con df_items en la columna 'id'/'item_id'
+    df_merged = pd.merge(df_genre, df_items, left_on='id', right_on='item_id')
+
+    # Agrupa por usuario y suma las horas jugadas
+    user_playtime = df_merged.groupby('user_id')['playtime_forever'].sum()
+
+    # Encuentra el usuario con más horas jugadas
+    max_playtime_user = user_playtime.idxmax()
+
+    # Agrupa por año y suma las horas jugadas
+    year_playtime = df_merged.groupby('year')['playtime_forever'].sum()
+
+    # Convierte year_playtime a una lista de diccionarios
+    playtime_list = [{"Año": year, "Horas": playtime} for year, playtime in year_playtime.items()]
+
+    return {"Usuario con más horas jugadas para el género {0}".format(genero): max_playtime_user, "Horas jugadas": playtime_list}
